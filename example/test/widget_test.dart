c69d98cb32172831b24cf0751e8a4b96198927ff
SalahProgramer <salahmustafanew904@gmail.com>
2025-03-11 14:05:25 +0200

    Clone, Setup, and Document Customers App and command readme

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,7 +8,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:amazon_payment_services_apple_pay_example/main.dart';
+// import 'package:amazon_payment_services_apple_pay_example/main.dart';
+
+import '../lib/main.dart';
 
 
 void main() {
